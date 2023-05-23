9590b6a8a57f4780662650d82438942363dfdeec
MikePio <michelepiopilla@gmail.com>
2023-05-24 01:12:07 +0200

    Added images and icons to use

--- index.php
+++ index.php
@@ -1,6 +1,97 @@
 <?php
 $test = '$test';
 
+// icona cane
+// <i class="fa-solid fa-dog"></i>
+
+// gatto
+// <i class="fa-solid fa-cat"></i>
+
+// fish
+// <i class="fa-solid fa-fish"></i>
+
+// uccello
+// <i class="fa-solid fa-crow"></i>
+
+// Cibo per cani
+// Categoria:
+// Cani
+// Prezzo:
+// 43.99
+// Peso netto:
+// 545g
+// Ingredienti:
+// Prosciutto, Riso
+
+// Cibo per cani 2
+// Categoria:
+// Cani
+// Prezzo:
+// 44.99
+// Peso netto:
+// 600g
+// Ingredienti:
+// Manzo, Cereali
+
+// Cibo per gatti
+// Categoria:
+// Gatti
+// Prezzo:
+// 34.99
+// Peso netto:
+// 400g
+// Ingredienti:
+// Tonno, Pollo, Prosciutto
+
+// Mangime per pesci
+// Categoria:
+// Pesci
+// Prezzo:
+// 2.95
+// Peso netto:
+// 30g
+// Ingredienti:
+// Ceriali, Lieviti, Alghe
+
+// Voliera in legno
+// Categoria:
+// Uccelli
+// Prezzo:
+// 184.99
+// Materiale:
+// Legno
+// Dimensioni:
+// 83 x 67x 153
+
+// Kong classic
+// Categoria:
+// Cani
+// Prezzo:
+// 13.49
+// Caratteristiche
+// Galleggia e rimbalza
+// Dimensioni:
+// 8.5 cm x 10 cm
+
+// Topo di peluche
+// Categoria:
+// Gatti
+// Prezzo:
+// 5
+// Caratteristiche
+// Morbido con codina in corda
+// Dimensioni:
+// 8.5 cm x 10 cm
+
+// Acquario
+// Categoria:
+// Pesci
+// Prezzo:
+// 55.99
+// Caratteristiche
+// Sistema di filtraggio integrato
+// Dimensioni:
+// L23 cm x W10 cm x H27.5 cm
 
 ?>
 
@@ -11,8 +102,9 @@ $test = '$test';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- font-awesome -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous'/>
   <!-- BOOTSTRAP -->
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">   -->
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
   <title>Pet Shop</title>
 </head>
@@ -21,7 +113,11 @@ $test = '$test';
   <h1>test</h1>
   <h2><?php echo $test ?></h2>
   
-  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script> -->
+<i class="fa-solid fa-crow"></i>
+<i class="fa-solid fa-dog"></i>
+<i class="fa-solid fa-cat"></i>
+<i class="fa-solid fa-fish"></i>
+
 
 
 </body>
