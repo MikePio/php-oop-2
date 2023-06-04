cbb80f0f64ef138bac80a10a46f5721b8bddc22d
MikePio <michelepiopilla@gmail.com>
2023-06-04 20:10:23 +0200

    Print the different parameters of classes on cards

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ $dogCrunchyBites = new Dog('Crunchy Bites', 'Dogs', '€43,99', 'img/img1.jpg', 
 //* da Dog che è il figlio di PetProduct.php
 $dogCrunchyBites->setNetWeight('545g');
 $dogCrunchyBites->setIngredients(['Ham', 'Rice']);
+$dogCrunchyBites->setCharacteristics(' - ');
+$dogCrunchyBites->setDimensions(' - ');
+
 
 //* Stampo l'oggetto (il prodotto)
 // var_dump($dogCrunchyBites);
@@ -22,30 +25,50 @@ $dogCrunchyBites->setIngredients(['Ham', 'Rice']);
 $dogSalmonPeas = new Dog('Salmon and Peas', 'Dogs', '€44,99', 'img/img2.webp', 'fa-solid fa-dog');
 $dogSalmonPeas->setNetWeight('600g');
 $dogSalmonPeas->setIngredients(['Salmon', 'Peas']);
+$dogSalmonPeas->setCharacteristics(' - ');
+$dogSalmonPeas->setDimensions(' - ');
+
 // Cibo per gatti
 $catChicken = new Cat('Chicken for cats', 'Cats', '€44,99', 'img/img3.png', 'fa-solid fa-cat');
 $catChicken->setNetWeight('400g');
-$catChicken->setIngredients('Chicken');
+$catChicken->setIngredients(['Chicken']);
+$catChicken->setCharacteristics(' - ');
+$catChicken->setDimensions(' - ');
+
 // Mangime per pesci
 $fishFeed = new Fish('Fish feed', 'Fishes', '€2,95', 'img/img4.png', 'fa-solid fa-fish');
 $fishFeed->setNetWeight('30g');
 $fishFeed->setIngredients(['Cereals', 'Yeasts', 'Algae']);
+$fishFeed->setMaterials([' - ']);
+$fishFeed->setDimensions(' - ');
+
 // Voliera in legno
 $birdAviary = new Bird('Bird aviary', 'Birds', '€184,99', 'img/img5.png', 'fa-solid fa-crow');
-$birdAviary->setMaterials('Wood');
+$birdAviary->setMaterials(['Wood']);
 $birdAviary->setDimensions('83 x 67x 153');
+
 // Kong classic
 $dogToy = new Dog('Kong classic, Dog toy', 'Dogs', '€13,49', 'img/img6.jpg', 'fa-solid fa-dog');
 $dogToy->setCharacteristics('Floats and bounces');
 $dogToy->setDimensions('8,5 cm x 10 cm');
+$dogToy->setIngredients([' - ']);
+$dogToy->setNetWeight(' - ');
+
 // Topo di peluche
 $catPlushMouse = new Cat('Plush mouse, Cat toy', 'Cats', '€4,99', 'img/img7.jpg', 'fa-solid fa-cat');
 $catPlushMouse->setCharacteristics('Soft with rope tail');
 $catPlushMouse->setDimensions('8,5 cm x 10 cm');
+$catPlushMouse->setIngredients([' - ']);
+$catPlushMouse->setNetWeight(' - ');
+
 // Acquario
 $fishAquarius = new Fish('Aquarius', 'Fishes', '€55,99', 'img/img8.jpg', 'fa-solid fa-fish');
 $fishAquarius->setMaterials(['Glass', 'Steel', 'Plastic']);
 $fishAquarius->setDimensions('L23 cm x W10 cm x H27,5 cm');
+$fishAquarius->setIngredients([' - ']);
+$fishAquarius->setNetWeight(' - ');
+
+
 
 // var_dump($dogSalmonPeas);
 // var_dump($catChicken);
@@ -101,12 +124,34 @@ $products = [
   <!-- //* stampare con un ciclo foreach l'array $products -->
   <?php  foreach ($products as $product) : ?>
     <div class="text-black my-3 ms-4 me-4" style="width: calc(100%/3.38);">
-      <div class="card" style="min-height: 575px;">
+      <div class="card" style="min-height: 642px;">
         <img src="<?php echo $product->getImage() ?>" class="card-img-top" alt="<?php echo $product->getTitle() ?>" style="height: 405px;">
         <div class="card-body">
           <h5 class="card-title"><?php echo $product->getTitle() ?></h5>
           <p class="card-text mb-1">Category: <i class="<?php echo $product->getIcon() ?>"></i> <?php echo $product->getCategory() ?></p>
           <p class="card-text mb-1">Price: <?php echo $product->getPrice() ?></p>
+          <?php if(get_class($product) == 'Dog') : ?>
+              <p class="card-text mb-1">Weight: <?php echo $product->netWeight ?></p>
+              <p class="card-text mb-1">Ingredients: <?php echo implode(', ', $product->ingredients)  ?></p>
+              <p class="card-text mb-1">Characteristics: <?php echo $product->characteristics ?></p>
+              <p class="card-text mb-1">Size: <?php echo $product->dimensions ?></p>
+          <?php endif; ?>
+          <?php if(get_class($product) == 'Cat') : ?>
+              <p class="card-text mb-1">Weight: <?php echo $product->netWeight ?></p>
+              <p class="card-text mb-1">Ingredients: <?php echo implode(', ', $product->ingredients) ?></p>
+              <p class="card-text mb-1">Characteristics: <?php echo $product->characteristics ?></p>
+              <p class="card-text mb-1">Size: <?php echo $product->dimensions ?></p>
+          <?php endif; ?>
+          <?php if(get_class($product) == 'Fish') : ?>
+              <p class="card-text mb-1">Weight: <?php echo $product->netWeight ?></p>
+              <p class="card-text mb-1">Ingredients: <?php echo implode(', ', $product->ingredients) ?></p>
+              <p class="card-text mb-1">Materials: <?php echo implode(', ', $product->materials) ?></p>
+              <p class="card-text mb-1">Size: <?php echo $product->dimensions ?></p>
+          <?php endif; ?>
+          <?php if(get_class($product) == 'Bird') : ?>
+              <p class="card-text mb-1">Materials: <?php echo implode(', ', $product->materials)  ?></p>
+              <p class="card-text mb-1">Size: <?php echo $product->dimensions ?></p>
+          <?php endif; ?>
         </div>
       </div>
     </div>
